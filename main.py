f8270507ea274a731940d55e34b228478dea2862
silenthill99 <florian.graziani@sfr.fr>
2023-10-05 22:53:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+
+
+class FileNotJsonFormatError(Exception):
+    def __init__(self):
+        self.message = "Veuillez faqire attention au format"
+
+
+def read_json_file(file_name):
+    try:
+        if not file_name.endswith('.json'):
+            raise FileNotJsonFormatError
+        fichier = open(file_name)
+        print(json.load(fichier))
+        fichier.close()
+    except FileNotFoundError:
+        print("Ce fichier n'existe pas !")
+
+read_json_file('bonjour.txt')
